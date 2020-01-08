0f8379bf82902fe0b139f0f55cb163b17d07f45d
Hakan-unal <hakanunal_1996@hotmail.com>
2020-01-08 23:25:57 +0300

    code updated

--- BST/binarySearchTree.js
+++ BST/binarySearchTree.js
@@ -1,4 +1,4 @@
-let input, tree, postorder, send, display;
+let input, tree, postorder, send, display, object;
 
 input = document.querySelector("#input");
 tree = document.querySelector("#tree");
@@ -17,10 +17,61 @@ class Node {
 }
 
 class BST {
+
     constructor() {
         this.root = null;
     }
+
+    insert(data) {
+        let newNode = new Node(data);
+        if (this.root === null) {
+            this.root = newNode;
+        }
+        else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+    insertNode(currentNode, newNode) {
+        if (newNode.data < currentNode.data) {
+            if (currentNode.left === null) {
+                currentNode.left = newNode;
+            }
+            else {
+                this.insertNode(currentNode.left, newNode);
+            }
+        }
+        else {
+            if (currentNode.right === null) {
+                currentNode.right = newNode;
+            }
+            else {
+                this.insertNode(currentNode.right, newNode);
+            }
+        }
+    }
+    postorder(node) {
+        if (node != null) {
+            this.postorder(node.left);
+            this.postorder(node.right);
+            postorder.value += node.data + " ";
+        }
+    }
 }
+
+
+object = new BST();
+
+send.addEventListener("click", () => {
+    let val;
+    val = Number(input.value);
+    object.insert(val);
+    input.value = "";
+    tree.value += val + ",";
+});
+
+display.addEventListener("click", () => {
+    object.postorder(object.root);
+});
 
 
 
